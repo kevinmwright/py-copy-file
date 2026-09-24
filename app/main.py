2be5a8d541570e9a9e3ce471407e998ccf674c01
kevinmwright <kevin.m.wright@outlook.com>
2026-09-24 15:08:09 +0200

    Solved

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+
+def copy_file(command: str) -> None:
+    tokens = command.split()
+    if len(tokens) < 3:
+        return
+    if tokens[0] != "cp":
+        return
+    if tokens[1] == tokens[2]:
+        return
+
+    try:
+        with open(tokens[1], "r") as file_in, open(tokens[2], "w") as file_out:
+            for line in file_in.readlines():
+                file_out.write(line)
+    except FileNotFoundError:
+        return
